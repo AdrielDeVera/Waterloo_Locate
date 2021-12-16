07e88efe8d947ff3ef9d143774b47364381e8a92
Jaiden-Wilson <96029895+Jaiden-Wilson@users.noreply.github.com>
2021-12-15 19:02:18 -0500

    Add files via upload

--- waterloo_locate.py
+++ waterloo_locate.py
@@ -1,12 +1,18 @@
-# uguwoogoo
-database = [
-#NOTE THIS IS JUST FOR TESTING
-	{"name":"Snorlax","human":False,"size":True},
-	{"name":"Ash","human":True,"size":False}
-
-]
-
-
+# Jaiden's waterloo_locate fork
+#Class containing a constructor method for each location in the game
+class locations:
+    def __init__(self,name,kind,ordinalPoint, faculty, age):
+        self.name=name
+        self.kind =kind
+        self.ordinalPoint = ordinalPoint
+        self.faculty=faculty
+        self.age=age
+#Instantiations of the locations class to be passed into the database  
+cmh = locations("claudette millar hall","academic","south east","engineering",4)
+pac = locations("physical activity centre","student services","north","n/a",1)
+dc = locations("davis centre","academic","north","mathematics",33)
+#database containing all distinct locations in the game
+database = [cmh,pac,dc]
 def take_chance(answer, property):
 	if answer == "y":
 		ans = True	
@@ -27,7 +33,7 @@ def take_chance(answer, property):
 #ADD VALIDATION/ERROR HANDLING
 def main():
 #QUESTIONS JUST FOR TESTING
-  ans = input("is the ans human?")
+  ans = input("Is your building more than 5 years old?")
   take_chance(ans,"human")
 
   ans = input("is the ans large?")
